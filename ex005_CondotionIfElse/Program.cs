aeb94976a6e40dbbe9b9266aa9e497c2abe0d915
yuri_k <krenev_yu@mail.ru>
2022-09-29 00:55:34 +0400

    ex005_CondotionIfElse

---
+++ ex005_CondotionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите Ваше имя: ");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "маша")
+{
+    Console.WriteLine ("Ура! Это снова ты!");
+}
+else
+{
+    Console.WriteLine ("Опять ты... Что надо?");
+    Console.WriteLine (userName);
+}
